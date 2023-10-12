20957eede0320b51345e1dda125803578d66cd9d
ikrammou <ikrammouraji@gmail.com>
2023-10-12 17:35:02 +0100

    tst

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CreatEmployeeComponent } from './components/creat-employee/creat-employee.component';
+import { EditEmployeeComponent } from './components/edit-employee/edit-employee.component';
+import { ListEmployeeComponent } from './components/list-employee/list-employee.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreatEmployeeComponent,
+    EditEmployeeComponent,
+    ListEmployeeComponent
   ],
   imports: [
     BrowserModule
